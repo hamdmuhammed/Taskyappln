e5161ac1c9577d36049f31b6b0372441cd3113c3
Hamd Muhammed <150013807+hamdmuhammed@users.noreply.github.com>
2024-01-23 12:29:40 +0000

    10000 yrs of Death

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var state={
-    taskList:[]
+    taskList: [],
 };
 
 // DOM objs
 var taskContents = document.querySelector(".task_contents");
 var taskModals = document.querySelector(".task__modal__body");
 
-var htmlTaskContent = ({id, url, title, description, type}) =>{`
+var htmlTaskContent = ({id, url, title, description, type}) =>`
     <div class='col-md-6 col-lg-4 mt-3' id=${id} key=${id}>
         <div class='card shadow-sm task__card'>
             <div class='card-header d-flex justify-content-end task__card__header'>
@@ -19,8 +19,10 @@ var htmlTaskContent = ({id, url, title, description, type}) =>{`
             </div>
             <div class="card-body">
               ${
-                url &&
+                url ?
                 `<img width="100%" src=${url} alt="card img cap" class="card-img-top md-3 rounded-lg" />`
+                :
+                `<img width="100%" src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.istockphoto.com%2Fid%2F1147544807%2Fvector%2Fthumbnail-image-vector-graphic.jpg%3Fs%3D612x612%26w%3D0%26k%3D20%26c%3DrnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM%3D&tbnid=E9-dfhF34W5_uM&vet=12ahUKEwiqqYSPvfODAxXfq2MGHeikA90QMygBegQIARB0..i&imgrefurl=https%3A%2F%2Fwww.istockphoto.com%2Fvector%2Fthumbnail-image-vector-graphic-gm1147544807-309589937&docid=ukljja2wttPDpM&w=612&h=612&q=image%20placeholder&ved=2ahUKEwiqqYSPvfODAxXfq2MGHeikA90QMygBegQIARB0" alt="card img cap" class="card-img-top md-3 rounded-lg" />`
               }
               <h4 class="card-title">${title}</h4>
               <p class="description trim-3-lines text-muted data-gram_editor='false'">${description}</p>
@@ -29,21 +31,22 @@ var htmlTaskContent = ({id, url, title, description, type}) =>{`
               </div>
             </div>
             <div class="card-footer">
-              <button type="button" class="btn btn-outline-primary float-right" data-bs-toggle="modal" data-bs-target="#ViewTaskModa">Open Task</button>
+              <button type="button" class="btn btn-outline-primary float-right" data-bs-toggle="modal" data-bs-target="#ViewTaskModal" onclick='opentask.apply(this, arguments)'>Open Task</button>
             </div>
         </div>
     </div>
 `;
-};
 
-var htmlTaskModal = (id, url, title, description, type) =>
+var htmlTaskModal = ({id, url, title, description}) =>
 {
   var date = new Date(parseInt(id));
   return`
     <div id=${id}>
       ${
-        url &&
+        url ?
        `<img width="100%" src=${url} alt="card img cap" class="card-img-top md-3 rounded-lg" />`
+        :
+        `<img width="100%" src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.istockphoto.com%2Fid%2F1147544807%2Fvector%2Fthumbnail-image-vector-graphic.jpg%3Fs%3D612x612%26w%3D0%26k%3D20%26c%3DrnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM%3D&tbnid=E9-dfhF34W5_uM&vet=12ahUKEwiqqYSPvfODAxXfq2MGHeikA90QMygBegQIARB0..i&imgrefurl=https%3A%2F%2Fwww.istockphoto.com%2Fvector%2Fthumbnail-image-vector-graphic-gm1147544807-309589937&docid=ukljja2wttPDpM&w=612&h=612&q=image%20placeholder&ved=2ahUKEwiqqYSPvfODAxXfq2MGHeikA90QMygBegQIARB0" alt="card img cap" class="card-img-top md-3 rounded-lg" />`
       }
       <strong class='text-sm text-muted'>Created on ${date.toDateString()}</strong>
       <h2 class='my-3'>${title}</h2>
@@ -54,13 +57,15 @@ var htmlTaskModal = (id, url, title, description, type) =>
 
 var updateLocalStorage = () =>
 {
-  localStorage.setItem('task', JSON.stringify({tasks : state.taskList,}))
+  localStorage.setItem('task', JSON.stringify({tasks: state.taskList,}))
 }
 
-var loadinitialData = () =>
+var loadInitialData = () =>
 {
   var localStorageCopy = JSON.parse(localStorage.tasks);
+
   if(localStorageCopy) state.taskList = localStorageCopy.tasks;
+  
   state.taskList.map((cardDate)=>
   {
     taskContents.insertAdjacentHTML("beforeend",htmlTaskContent(cardDate));
@@ -74,6 +79,27 @@ var handleSubmit = (event) =>
     url: document.getElementById("imageURL").value,
     title: document.getElementById("taskTitle").value,
     type: document.getElementById("tags").value,
-    description: document.getElementById("description").value,
+    description: document.getElementById("taskDescription").value,
+  };
+
+  if(input.title === "" || input.type === "" || input.description === "")
+  {
+    return alert("Please Fill All The Fields")
   }
+
+  // spread operator
+  taskContents.insertAdjacentHTML("beforeend",htmlTaskContent({
+    ...input, id
+  }));
+
+  state.taskList.push({...input, id});
+  updateLocalStorage()
+};
+
+var opentask = (e) =>
+{
+  if (!e) e = window.event;
+
+  var getTask = state.taskList.find(({id})=> id === e.target.id);
+  taskModals.innerHTML = htmlTaskModal(getTask)
 }
